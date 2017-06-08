64445b3f3a276c41a1e31d613d45bcae87517a41
Yoav Kleinberger <haarcuba@gmail.com>
2017-06-08 19:07:11 +0300

    version 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README = 'linux network helpers - e.g. list interfaces and their IP addresses'
 requires = []
 
 setup(name='linux-nethelp',
-      version='1.0.0',
+      version='1.0.1',
       description=README,
       long_description=README,
       url='https://github.com/haarcuba/linux-nethelp',
